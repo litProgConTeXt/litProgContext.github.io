3fea768f8b60f4b25e10b202628f33adc1a08986
Stephen Gaito <stephen@perceptisys.co.uk>
2023-01-03 07:47:26 +0000

    Nikola auto commit.
    Nikola version: 8.2.3

--- conf.py
+++ conf.py
@@ -229,7 +229,10 @@ TIMEZONE = "UTC"
 # One or more folders containing files to be copied as-is into the output.
 # The format is a dictionary of {source: relative destination}.
 # Default is:
-# FILES_FOLDERS = {'files': ''}
+FILES_FOLDERS = {
+  'files': '',
+  'pages/notes' : 'notes'
+}
 # Which means copy 'files' into 'output'
 
 # One or more folders containing code listings to be processed and published on
